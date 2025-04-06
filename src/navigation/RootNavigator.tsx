ce59c66819618269fc930a561b3562ceb992fb50
VasudevaSurf <vasudev@surf.mt>
2025-04-06 21:29:50 +0530

    Small change in root Navigation

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,20 +1,22 @@
-import {createStackNavigator} from '@react-navigation/stack';
-import React, {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {checkAuthStatus} from '../redux/slices/authSlice';
-import type {AppDispatch, RootState} from '../redux/store';
-import {RootStackParamList} from '../types/navigation';
-import {AuthNavigator} from './stacks/AuthNavigator';
-import {CreateNavigator} from './stacks/CreateNavigator';
-import {DashboardNavigator} from './stacks/DashBoardNavigator';
-import {OnboardingNavigator} from './stacks/OnboardingNavigator';
-import {VATNavigator} from './stacks/VATNavigator';
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { checkAuthStatus } from "../redux/slices/authSlice";
+import type { AppDispatch, RootState } from "../redux/store";
+import { RootStackParamList } from "../types/navigation";
+import { AuthNavigator } from "./stacks/AuthNavigator";
+import { CreateNavigator } from "./stacks/CreateNavigator";
+import { DashboardNavigator } from "./stacks/DashBoardNavigator";
+import { OnboardingNavigator } from "./stacks/OnboardingNavigator";
+import { VATNavigator } from "./stacks/VATNavigator";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export const RootNavigator = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const {isLoggedIn, isLoading} = useSelector((state: RootState) => state.auth);
+  const { isLoggedIn, isLoading } = useSelector(
+    (state: RootState) => state.auth
+  );
   const [isInitialized, setIsInitialized] = useState(false);
 
   // Check auth status when app starts
@@ -31,7 +33,8 @@ export const RootNavigator = () => {
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
-      }}>
+      }}
+    >
       {!isLoggedIn ? (
         <>
           <Stack.Screen name="Onboarding" component={OnboardingNavigator} />
@@ -42,6 +45,7 @@ export const RootNavigator = () => {
       ) : (
         <>
           <Stack.Screen name="Dashboard" component={DashboardNavigator} />
+          <Stack.Screen name="Auth" component={AuthNavigator} />
         </>
       )}
     </Stack.Navigator>
